a90b60d64529a44e16abd98843cce5bc7c43a79a
Hazrat Ali <iloveyii@yahoo.com>
2020-06-24 12:41:14 +0200

    update a user

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -2,19 +2,37 @@ const User = require("@sequelize/models").User;
 
 // @desc   Get all from User
 // @route  GET /api/v1/user
-export const getUser = (req: any, res: any, next: any) => {
+export const getUsers = async (req: any, res: any, next: any) => {
     try {
         console.log("API User : GET /api/v1/user");
         const user = [{id: 1, name: "aaaa"}, {id: 2, name: "bbb"}];
-        /*return res.status(200).json({
+        const models = await User.findAll();
+        return res.status(200).send({
             success: true,
-            data: user
-        }); */
+            data: models
+        });
 
-        User.findAll()
-            .then((users: any[]) => {
-                res.status(200).send(users);
-            });
+    } catch (error) {
+        res.send(500).json({
+            success: false,
+            error: "Server error"
+        });
+    }
+};
+
+
+// @desc   Get a User
+// @route  GET /api/v1/user/:id
+export const getUser = async (req: any, res: any, next: any) => {
+    try {
+        console.log("API User : GET /api/v1/user/:id");
+        const {id} = req.params;
+        const model = await User.findOne({where: {id}});
+        console.log(model);
+        return res.status(200).send({
+            success: true,
+            data: model,
+        });
 
     } catch (error) {
         res.send(500).json({
@@ -35,10 +53,9 @@ export const addUser = async (req: any, res: any, next: any) => {
         console.log({email, password});
         const user: any = await User.create({email, password});
         console.log("API User : POST /api/v1/user");
-        return res.status(201).json({
+        return res.status(201).send({
             success: true,
-            data: user.getDataValue,
-            status
+            data: user,
         });
     } catch (error) {
         res.send(500).json({
@@ -60,14 +77,13 @@ export const updateUser = async (req: any, res: any, next: any) => {
         };
         console.log({email, password});
         const model = await User.findOne({where: {id}});
-        const status = model.update({
+        const status = await model.update({
             email, password
         });
 
-        const data = [{id, status}];
-        return res.status(200).json({
+        return res.status(200).send({
             success: true,
-            data
+            data: model
         });
     } catch (error) {
         res.send(500).json({
@@ -84,7 +100,7 @@ export const deleteUser = async (req: any, res: any, next: any) => {
         const {id} = req.params;
         const status = await User.destroy({where: {id}});
         const user = [{id, name: "delete", status}];
-        return res.status(200).json({
+        return res.status(200).send({
             success: true,
             data: user
         });
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,14 +1,15 @@
 import express from "express";
-import { getUser, addUser, updateUser, deleteUser } from "../controllers/user";
+import { getUser, getUsers, addUser, updateUser, deleteUser } from "../controllers/user";
 
 const router = express.Router();
 
-router.route("/")
-    .get(getUser)
-    .post(addUser);
-
 router.route("/:id")
+    .get(getUser)
     .delete(deleteUser)
     .put(updateUser);
+
+router.route("/")
+    .get(getUsers)
+    .post(addUser);
 
 export default router;
