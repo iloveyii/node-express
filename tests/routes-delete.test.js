91bf5e5bc491620982a128df4e0fb9a002c691e2
Hazrat Ali <iloveyii@yahoo.com>
2020-06-28 15:28:29 +0200

    Test - delete a user with a token

--- tests/routes-delete.test.js
+++ tests/routes-delete.test.js
@@ -8,27 +8,12 @@ require('dotenv').config();
 const PORT = process.env.NODE_ENV.PORT || 6600;
 request = request.bind(request, `http://localhost:${PORT}`);
 
-describe('/GET users', () => {
-    it('it should get all the users', async () => {
-        const res = await request(null)
-            .get('/api/v1/users')
-            .send({
-                userId: 1,
-                title: 'test is cool',
-            })
-        expect(res.statusCode).toEqual(200)
-        expect(res.body).toHaveProperty('success')
-        expect(res.body).toHaveProperty('data')
-        expect(res.body.data.length).toBeGreaterThan(0)
-    });
-});
 
-
-describe('/GET a user with id', () => {
+describe('/DELETE a user with id', () => {
     let token = null;
-    it('it should not get the user without token', async () => {
+    it('it should not delete the user without a token', async () => {
         const res = await request(null)
-            .get('/api/v1/users/1')
+            .delete('/api/v1/users/1')
             .send({})
         expect(res.statusCode).toEqual(403)
         expect(res.body).toHaveProperty('success')
@@ -36,7 +21,7 @@ describe('/GET a user with id', () => {
         expect(res.body.success).toBeFalsy()
     });
 
-    it('it should get the use log in and get a token', async () => {
+    it('it should get the user log in and get a token', async () => {
         const res = await request(null)
             .post('/api/v1/login')
             .send(
@@ -52,20 +37,17 @@ describe('/GET a user with id', () => {
         expect(res.body).toHaveProperty('data')
         expect(res.body.data).toHaveProperty('token')
         token = res.body.data.token;
-
     });
 
 
-    it('it should get the user with a token', async () => {
+    it('it should delete the user with the token', async () => {
         const res = await request(null)
-            .get('/api/v1/users/1')
+            .delete('/api/v1/users/1')
             .set('Authorization', 'bearer ' + token)
             .send()
         expect(res.statusCode).toEqual(200)
         expect(res.body).toHaveProperty('success')
         expect(res.body).toHaveProperty('data')
-        expect(res.body.data).toHaveProperty('id')
-        expect(res.body.data).toHaveProperty('email')
         expect(res.body.success).toBeTruthy()
     });
 });
