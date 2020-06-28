df071b5afa7dc0cec48e7015c7d5ed98a5a1e5c8
Hazrat Ali <iloveyii@yahoo.com>
2020-06-28 15:20:40 +0200

    Test - login user

--- tests/routes.test.js
+++ tests/routes.test.js
@@ -1,6 +1,9 @@
 let request = require('supertest');
 require('dotenv').config();
 
+// -------------------------------------------------------------------
+// API Routes - DB must contain tables - Or run sequelize/npm run test
+// -------------------------------------------------------------------
 
 const PORT = process.env.NODE_ENV.PORT || 6600;
 request = request.bind(request, `http://localhost:${PORT}`);
@@ -17,5 +20,57 @@ describe('/GET users', () => {
         expect(res.body).toHaveProperty('success')
         expect(res.body).toHaveProperty('data')
         expect(res.body.data.length).toBeGreaterThan(0)
-    })
-})
+    });
+});
+
+
+describe('/GET a user with id', () => {
+    let token = null;
+    it('it should not get the user without token', async () => {
+        const res = await request(null)
+            .get('/api/v1/users/1')
+            .send({})
+        expect(res.statusCode).toEqual(403)
+        expect(res.body).toHaveProperty('success')
+        expect(res.body).toHaveProperty('error')
+        expect(res.body.success).toBeFalsy()
+    });
+
+    it('it should get the use log in and get a token', async () => {
+        const res = await request(null)
+            .post('/api/v1/login')
+            .send(
+                {
+                    "user": {
+                        "email": "user1@email.com",
+                        "password": "pass1"
+                    }
+                }
+            )
+        expect(res.statusCode).toEqual(200)
+        expect(res.body).toHaveProperty('success')
+        expect(res.body).toHaveProperty('data')
+        expect(res.body.data).toHaveProperty('token')
+        token = res.body.data.token;
+
+    });
+
+
+    it('it should get the user with a token', async () => {
+        const res = await request(null)
+            .get('/api/v1/users/1')
+            .set('Authorization', 'bearer ' + token)
+            .send({
+                Authorization: 'Bearer ' + token
+            })
+        expect(res.statusCode).toEqual(200)
+        expect(res.body).toHaveProperty('success')
+        expect(res.body).toHaveProperty('data')
+        expect(res.body.data).toHaveProperty('id')
+        expect(res.body.data).toHaveProperty('email')
+        expect(res.body.success).toBeTruthy()
+        console.log(res.body.data)
+    });
+});
+
+
