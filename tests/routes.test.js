625a85d78741570a844bb58f3af7fd28b14e3f64
Hazrat Ali <iloveyii@yahoo.com>
2020-06-28 14:43:39 +0200

    Added routes test

--- tests/routes.test.js
+++ tests/routes.test.js
@@ -1,14 +1,21 @@
-const request = require('supertest')
-const app = require('../sequelize/src/app')
-describe('Post Endpoints', () => {
-    it('should create a new post', async () => {
-        const res = await request(app)
+let request = require('supertest');
+require('dotenv').config();
+
+
+const PORT = process.env.NODE_ENV.PORT || 6600;
+request = request.bind(request, `http://localhost:${PORT}`);
+
+describe('/GET users', () => {
+    it('it should get all the users', async () => {
+        const res = await request(null)
             .get('/api/v1/users')
             .send({
                 userId: 1,
                 title: 'test is cool',
             })
         expect(res.statusCode).toEqual(200)
-        // expect(res.body).toHaveProperty('post')
+        expect(res.body).toHaveProperty('success')
+        expect(res.body).toHaveProperty('data')
+        expect(res.body.data.length).toBeGreaterThan(0)
     })
 })
