636083b7e02e24b68399baed1687d5c008de64a3
Hazrat Ali <iloveyii@yahoo.com>
2020-06-30 18:49:13 +0200

    added find and create to mongo.ts

--- src/models/Mongo.ts
+++ src/models/Mongo.ts
@@ -1,21 +1,104 @@
 import { MongoClient } from "mongodb";
 
+type ConditionT = {
+    where?: any;
+};
 
-// Defining schema interface
-interface UserSchema {
-    _id: { $oid: string };
-    username: string;
-    password: string;
-}
+type UserT = {
+    id?: string | number;
+    email: string;
+    password: string
+};
 
-// const db = client.database("test");
-// const users = db.collection<UserSchema>("users");
+type ResultT = {
+    success: boolean;
+    data: UserT;
+};
 
 const mongo = {
     dbname: "shop",
     url: "mongodb://localhost:27017",
-    mongoOptions: {useNewUrlParser: true},
+    mongoOptions: {useNewUrlParser: true, useUnifiedTopology: true},
 };
+
+interface UserI {
+
+    findOne(condition: ConditionT): any;
+
+    findAll(condition: ConditionT): any;
+
+    create(user: UserT): any;
+
+    update(user: any): any;
+
+    delete(condition: ConditionT): any;
+}
+
+class Model implements UserI {
+    isNewRecord: boolean = true;
+    user: UserT | undefined = undefined;
+    // for response
+    success: boolean = true;
+    data: any = undefined;
+    // for dialect
+    collection: any = undefined;
+
+    constructor(user?: UserT) {
+        this.user = user;
+        if (user) this.isNewRecord = true;
+    }
+
+    // Connect to db
+    async connect() {
+        if (this.collection !== undefined) return this.collection;
+        try {
+            const client = await MongoClient.connect(mongo.url, mongo.mongoOptions);
+            console.log("Mongodb connected to : " + mongo.dbname);
+            const db = client.db(mongo.dbname);
+            this.collection = db.collection("users");
+        } catch (error) {
+            console.log("Error : ", error);
+        }
+    }
+
+    async findOne(condition: ConditionT) {
+        await this.connect();
+        const model = await this.collection.find(condition);
+        this.data = {
+            model: model.toArray()
+        };
+        return model;
+    }
+
+    async findAll(condition: ConditionT) {
+        await this.connect();
+        console.log('"this data : ', this.data);
+        const model = await this.collection.find(condition);
+        this.data = {
+            models: await model.toArray()
+        };
+        console.log('"this data : ', this.data);
+        return model;
+    }
+
+    async create(user: UserT) {
+        await this.connect();
+        const model = await this.collection.insertOne(user);
+        this.data = {
+            model: model.ops[0]
+        };
+        return model;
+    }
+
+    async update(user: any) {
+        await this.connect();
+
+    }
+
+    async delete(condition: ConditionT) {
+        await this.connect();
+    }
+}
 
 
 function connect() {
@@ -34,5 +117,13 @@ function connect() {
     });
 }
 
-connect();
 
+async function test_db() {
+    const m = await new Model();
+    await m.findAll({});
+    await m.create({email: "em2", password: "pass2"});
+    console.log(" m : ", m.data);
+}
+
+
+test_db();
