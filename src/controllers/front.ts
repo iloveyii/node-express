b851fecd94268963b74bc38857db8ae91091d987
Hazrat Ali <iloveyii@yahoo.com>
2020-07-10 11:35:05 +0200

    Added details

--- src/controllers/front.ts
+++ src/controllers/front.ts
@@ -18,6 +18,9 @@ export const getArticlesPage = async (req: Request, res: Response, next: NextFun
         case "carousel":
             res.render("front/partials/_carousel.ejs");
             break;
+        case "details":
+            res.render("front/partials/_details.ejs");
+            break;
         default:
             res.status(404).render("front/404.ejs", {id: req.params.id});
     }
