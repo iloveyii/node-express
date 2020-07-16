075a9ff3ec5645453aeea53437c5cca81cd141eb
Hazrat Ali <iloveyii@yahoo.com>
2020-07-16 14:14:13 +0200

    Added articles to views

--- src/controllers/front.ts
+++ src/controllers/front.ts
@@ -3,15 +3,19 @@ import { Request, Response, NextFunction } from "express";
 import { Database } from "../models/base/Database";
 import Condition from "../models/base/Condition";
 import Product from "../models/Product";
+import Article from "../models/Article";
 
 
-const database = new Database("blog");
+const database = new Database("shop");
 
 // @desc   Get all from Model
 // @route  GET /api/v1/product
 export const getPage = async (req: Request, res: Response, next: NextFunction) => {
     console.log("Params : ", req.params);
-    const options = {extractStyles: true, extractScripts: true, layout: "front/layout"};
+    const model = new Article(database, "articles", undefined);
+    await model.read();
+    const response = model.response;
+    const options = {extractStyles: true, extractScripts: true, layout: "front/layout", articles: response.data};
     switch (req.params?.id) {
         case "index":
             res.render("front/articles.ejs", options);
